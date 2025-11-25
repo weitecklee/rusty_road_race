a30dee9f83765a1ea89a84620a31e521c9bb335d
arkteck <arkteck@gmail.com>
2025-11-25 13:04:04 -0500

    obstacles

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 use rand::{distr::Uniform, prelude::*};
-use rusty_engine::prelude::*;
+use rusty_engine::prelude::{bevy::sprite::Sprite, *};
 
 #[derive(Resource)]
 struct GameState {
     health_amount: u8,
     lost: bool,
+    x_values: Uniform<f32>,
     y_values: Uniform<f32>,
 }
 
 const PLAYER_SPEED: f32 = 250.0;
 const ROAD_SPEED: f32 = 400.0;
+const OBSTACLE_SPEED: f32 = 600.0;
 
 fn main() {
     let mut game = Game::new();
     let mut rng = rand::rng();
-    let x_values = Uniform::new(-640.0, 640.0).unwrap();
+    let x_values = Uniform::new(800.0, 1600.0).unwrap();
     let y_values = Uniform::new(-360.0, 360.0).unwrap();
 
     game.window_settings(Window {
@@ -32,17 +34,36 @@ fn main() {
     player1.collision = true;
 
     for i in 0..rng.random_range(8..=17) {
-        let label = format!("roadline{}", i);
-        let roadline = game.add_sprite(label, SpritePreset::RacingBarrierWhite);
+        let roadline = game.add_sprite(format!("roadline{}", i), SpritePreset::RacingBarrierWhite);
         roadline.scale = 0.1;
-        roadline.translation.x = rng.sample(x_values);
+        roadline.translation.x = rng.random_range(-640.0..=640.0);
         roadline.translation.y = rng.sample(y_values);
+    }
+
+    let obstacle_presets = [
+        SpritePreset::RacingBarrelBlue,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RollingBallBlue,
+        SpritePreset::RollingBallRed,
+        SpritePreset::RacingConeStraight,
+        SpritePreset::RollingBallRedAlt,
+        SpritePreset::RollingBallBlueAlt,
+        SpritePreset::RollingBlockSquare,
+    ];
+
+    for (i, preset) in obstacle_presets.into_iter().enumerate() {
+        let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
+        obstacle.layer = 5.0;
+        obstacle.collision = true;
+        obstacle.translation.x = rng.sample(x_values);
+        obstacle.translation.y = rng.sample(y_values);
     }
 
     game.add_logic(game_logic);
     game.run(GameState {
         health_amount: 5,
         lost: false,
+        x_values,
         y_values,
     });
 }
@@ -88,7 +109,13 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
         if sprite.label.starts_with("roadline") {
             sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
             if sprite.translation.x < -675.0 {
-                sprite.translation.x += 1500.0;
+                sprite.translation.x = rng.sample(game_state.x_values);
+                sprite.translation.y = rng.sample(game_state.y_values);
+            }
+        } else if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= OBSTACLE_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = rng.sample(game_state.x_values);
                 sprite.translation.y = rng.sample(game_state.y_values);
             }
         }
