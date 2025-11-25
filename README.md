f32ef0eee4b94fd8a8818c00c009be3a342971fc
arkteck <arkteck@gmail.com>
2025-11-25 11:15:51 -0500

    game setup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rusty Road Race
 
 This repository contains code generated for the [Road Race](https://github.com/CleanCut/rusty_engine/blob/main/scenarios/road_race.md) game scenario as part of the [Rusty Engine Tutorial](https://cleancut.github.io/rusty_engine/).
+
+Additional assets are from [Ravenmore](https://ravenmore.itch.io/free-space-shooter-assets).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {
+    health_amount: u8,
+    lost: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    // game setup goes here
+
+    game.add_logic(game_logic);
+    game.run(GameState {
+        health_amount: 5,
+        lost: false,
+    });
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // game logic goes here
 }
